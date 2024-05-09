c1c9c0f3abb38411c74fb7b7e28270c9c195e5d4
ebarsula <ebarsulai@gmail.com>
2024-05-09 14:54:26 +0300

    long double implement

--- main.c
+++ main.c
@@ -1,16 +1,43 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+#include <errno.h> 
+#include <string.h>
 
-int compare(const void *a, const void *b) {
-    return (*(int*)a - *(int*)b);
+
+void sort(int arr[], int size)
+{
+    for (int i = 0; i < (size - 1); i++)
+    {
+        for (int j = 0; j < (size - i - 1); j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
 
 
 int main(int argc, char* argv[]){
-     FILE *file_read;
-    char line[100];
-    file_read = fopen("data.txt", "r");
+    FILE *file_read;
+    char line[100]; 
+    if (argc != 2) {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return (-1);
+    }
+    if (strcmp(argv[1], "data.txt") != 0) {
+       printf("use file name data.txt\n");
+        return (-1);
+    }
+    file_read = fopen(argv[1], "r");
+     if (file_read == NULL) {
+       printf("Error opening the file\n");
+        return (-1);
+    }
     int line_num = 0;
     int i = 0;
     while(fgets(line, sizeof(line), file_read)){
@@ -22,30 +49,30 @@ int main(int argc, char* argv[]){
     while(fgets(line, sizeof(line), file_read)){
         
     
-        int num = atoi(line);
+        long double num = atoi(line);
         num_array[i++] = num;
     }
-    qsort(num_array, line_num, sizeof(int), compare);
+    sort(num_array, line_num);
  
-   int median = 0;
-   int variance = 0;
-   int total = 0;
+   long double median = 0;
+   long double variance = 0;
+   long double total = 0;
  
      for ( int i = 0; i < line_num; i++){
         total += num_array[i];
       
      }
-     printf("%d total \n",total);
-     printf("%d line num \n",line_num);
-    int  average = total/line_num;
+    //  printf("%d total \n",total);
+    //  printf("%d line num \n",line_num);
+    long double  average = total/line_num;
       for ( int i = 0; i < line_num; i++){
       
         variance += (num_array[i]-average) * (num_array[i]-average);
        
        
      }
-     int s = variance/line_num;
-     double std_dev = sqrt(s);
+     variance = variance/line_num;
+     long double std_dev = sqrt(variance);
     
      if (line_num % 2 == 0) {
         median = (num_array[line_num/2] + num_array[line_num/2 + 1]) / 2;
@@ -53,10 +80,10 @@ int main(int argc, char* argv[]){
         median = num_array[line_num/2];
      }
 
-     printf("Average: %d \n", average);
-      printf("Median %d \n", median);
-        printf("Variance %d \n", variance/line_num);
-         printf("Standard Deviation %f \n", std_dev);
+    printf("Average: %.0Lf\n", (long double)round(average));
+    printf("Median: %.0Lf\n", (long double)ceil(median));
+    printf("Variance: %.0Lf\n", (long double)round(variance));
+    printf("Standard Deviation %.0Lf\n", (long double)round(std_dev));
     fclose(file_read);
   
     
